21a63a6467813eb43331ae6598df2f77327fdfae
Baran Öner <baranoner5@gmail.com>
2024-07-10 14:04:48 +0300

    Upgrade sistemi rastgeleligi duzeltildi

--- assets/Scriptable Objects/Upgrades/UpgradeManager.cs
+++ assets/Scriptable Objects/Upgrades/UpgradeManager.cs
@@ -7,25 +7,43 @@ public class UpgradeManager : MonoBehaviour
 
     public Upgrade[] upgrades;
     public GameObject[] buttons;
-    // Start is called before the first frame update
+    public float defaultDamageAmount = 5;
+    [SerializeField] Float damageAmount;
+    private int maxNum;
+    private List <int> randomList;
+
+
+    
     void Start()
     {
-        
-       
+     maxNum = upgrades.Length;   
+     damageAmount.Value = defaultDamageAmount;  
         
 
 
     }
 
     public void PrepareUpgradePanel(){
+        randomList = new List<int>();
+        GenerateRandomList();
+        
         for(int i = 0; i<buttons.Length; i++){
-            int random = Random.Range(0, upgrades.Length);
-            buttons[i].GetComponent<UpgradeBtn>().SetUpgrade(upgrades[random].name, upgrades[random].description, 
-            upgrades[random].level, upgrades[random].artwork, upgrades[random].healthChange, upgrades[random].attackChange, upgrades[random].speedChange);
+            
+            buttons[i].GetComponent<UpgradeBtn>().SetUpgrade(upgrades[randomList[i]].name, upgrades[randomList[i]].description, 
+            upgrades[randomList[i]].level, upgrades[randomList[i]].artwork, upgrades[randomList[i]].healthChange, upgrades[randomList[i]].attackChange, upgrades[randomList[i]].speedChange);
         }
     }
 
-    // Update is called once per frame
+    public void GenerateRandomList (){
+    for(int i = 0; i < maxNum; i++){
+       int numToAdd = Random.Range(0,maxNum);
+       while(randomList.Contains(numToAdd)){
+          numToAdd = Random.Range(0,maxNum);
+       }
+       randomList.Add(numToAdd);
+
+   }
+    }
     void Update()
     {
         
